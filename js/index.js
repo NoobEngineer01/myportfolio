bcb7662dda25cb7064bb4a58aa7f6333b1e0c435
Dishant Gupta <85665886+whitewolf-developer@users.noreply.github.com>
2022-09-07 10:55:45 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+/*====================Typing Animation==================*/
+var typed = new Typed(".typing",{
+    strings: ["","Front-End Developer.", "Back-End Developer.", "Salesforce Developer." ],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+});
